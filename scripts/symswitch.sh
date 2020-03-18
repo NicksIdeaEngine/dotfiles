4eedadcff50bd5f9d165f7d0ace26e2067bb00f0
Nick Garcia <NicksIdeaEngine@gmail.com>
2020-03-18 01:02:56 -0400

    Symswitch tweak

--- scripts/symswitch.sh
+++ scripts/symswitch.sh
@@ -7,5 +7,7 @@ DEST_FILE="$DEST_DIR/$1"
 if [[ ! -f "$DEST_FILE" ]]; then
   mv "$SOURCE_DIR/$1" "$DEST_FILE"
   ln -s "$DEST_FILE" "$SOURCE_DIR"
+elif [[ ! -f "$SOURCE_DIR/$1" ]]; then
+  ln -s "$DEST_FILE" "$SOURCE_DIR"
 fi
 
