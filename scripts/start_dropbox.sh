a02026f8a5916a86e7c607251f35219dbeae3e6a
Nick Garcia <NicksIdeaEngine@gmail.com>
2020-04-14 12:06:53 -0400

    Adding logging to start_dropbox.sh

--- scripts/start_dropbox.sh
+++ scripts/start_dropbox.sh
@@ -4,16 +4,29 @@ dbx_dir=/mnt/sdc1/dbx
 filter_file=/home/efex/.config/rclone/filter-from.txt
 remote=dbx
 backup=/mnt/sdc1/.rclone-mount-cache
+synclog=/home/efex/rclone-sync.log
+copylog=/home/efex/rclone-copy.log
 
-rclone copy -uv \
+if [[ -f $copylog ]]; then
+  rm $copylog
+fi
+
+if [[ -f $synclog ]]; then
+  rm $synclog
+fi
+
+rclone copy -uvv \
   --create-empty-src-dirs \
   $remote: $dbx_dir \
   --backup-dir $backup \
   --suffix .$(date +"%Y-%m-%d-%H-%M-%S") \
   --filter-from $filter_file \
+  --log-file="$copylog" \
   && find $dbx_dir | \
   entr -d -r \
-  rclone sync -v \
+  rclone sync -vv \
   --create-empty-src-dirs \
   $dbx_dir $remote: \
-  --filter-from $filter_file
+  --filter-from $filter_file \
+  --log-file="$synclog"
+
